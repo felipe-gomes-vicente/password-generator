92786bea980bc2f19e5983075c658a1628202fd2
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-09-07 19:26:15 -0300

    refactoring display of the copied text message

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,5 +73,5 @@ const generatePassword = (lower, upper, number, symbol, length) => {
 
 btnCopy.addEventListener("click", () => {
   navigator.clipboard.writeText(result.value);
-  alert("texto copiado" + result.value);
+  alert("Texto copiado " + result.value);
 });
